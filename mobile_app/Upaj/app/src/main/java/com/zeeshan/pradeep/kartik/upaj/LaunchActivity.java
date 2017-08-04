67ffd151c685a906de8f9fd474ba495e33a0ce7e
kartik <kartik.gupta@iitrpr.ac.in>
2017-08-04 22:51:49 +0530

    app launch done

---
+++ mobile_app/Upaj/app/src/main/java/com/zeeshan/pradeep/kartik/upaj/LaunchActivity.java
@@ -0,0 +1,54 @@
+package com.zeeshan.pradeep.kartik.upaj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LaunchActivity extends AppCompatActivity {
+
+
+    private static final int AADHAR_NO_LENGTH = 16;
+    EditText mAadharEditText;
+    Button mProceedButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+
+        mAadharEditText = (EditText) findViewById(R.id.aadharEditText);
+        mProceedButton = (Button) findViewById(R.id.proceedButton);
+
+
+        mProceedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = mAadharEditText.getText().toString();
+
+                if (inputText.length() != AADHAR_NO_LENGTH)
+                {
+                    Toast.makeText(getApplicationContext(), "please enter a proper Aadhaar no", Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    authenticateAadhar(inputText);
+
+                }
+            }
+        });
+
+
+
+
+    }
+
+    private void authenticateAadhar(String inputText) {
+        //TODO:needs to be implemented
+    }
+}
