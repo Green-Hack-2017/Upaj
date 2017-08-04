864ff63d9d2b3f6867faf664f8d4220a4bdefb8a
kartik <kartik.gupta@iitrpr.ac.in>
2017-08-05 02:47:58 +0530

    reduced min sdk to 17 and updated the helpline fragment

--- mobile_app/Upaj/app/src/main/java/com/zeeshan/pradeep/kartik/upaj/HelpFragment.java
+++ mobile_app/Upaj/app/src/main/java/com/zeeshan/pradeep/kartik/upaj/HelpFragment.java
@@ -1,11 +1,15 @@
 package com.zeeshan.pradeep.kartik.upaj;
 
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,17 +17,34 @@ import android.view.ViewGroup;
  */
 public class HelpFragment extends Fragment {
 
+Button mCallHelpLineButton;
 
     public HelpFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+        mCallHelpLineButton = (Button) view.findViewById(R.id.callButton);
+
+
+        mCallHelpLineButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                String number = "5464564646546";
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + number));
+                startActivity(callIntent);
+            }
+
+        });
+
+        return view;
     }
+
 
 }
