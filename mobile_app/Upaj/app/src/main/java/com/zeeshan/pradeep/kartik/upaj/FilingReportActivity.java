77e1c5bbab81466005d4c0e4c2a3e3b61ff1e86d
kartik <kartik.gupta@iitrpr.ac.in>
2017-08-05 12:54:30 +0530

    retrieve image data from camera app

--- mobile_app/Upaj/app/src/main/java/com/zeeshan/pradeep/kartik/upaj/FilingReportActivity.java
+++ mobile_app/Upaj/app/src/main/java/com/zeeshan/pradeep/kartik/upaj/FilingReportActivity.java
@@ -1,13 +1,31 @@
 package com.zeeshan.pradeep.kartik.upaj;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class FilingReportActivity extends AppCompatActivity {
+
+    private static final String CURRENT_IMAGE_PATH = "current_image_path";
+
+    ImageView mCurrentPhotoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filing_report);
+
+        mCurrentPhotoImageView = (ImageView) findViewById(R.id.currentPhotoImageView);
+
+        String imagePath=getIntent().getStringExtra(CURRENT_IMAGE_PATH);
+
+
+        Bitmap bmImg = BitmapFactory.decodeFile(String.valueOf(imagePath));
+        mCurrentPhotoImageView.setImageBitmap(bmImg);
+
+
+
     }
 }
--- mobile_app/Upaj/app/src/main/java/com/zeeshan/pradeep/kartik/upaj/ReportFragment.java
+++ mobile_app/Upaj/app/src/main/java/com/zeeshan/pradeep/kartik/upaj/ReportFragment.java
@@ -6,17 +6,25 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
@@ -26,8 +34,12 @@ public class ReportFragment extends Fragment {
 
 
     private static final String TAG = ReportFragment.class.getSimpleName();
+    private static final String CURRENT_IMAGE_PATH = "current_image_path";
+
     Button mReportButton;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     public ReportFragment() {
@@ -83,17 +95,57 @@ public class ReportFragment extends Fragment {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
             Log.d(TAG,"result of resolve activity is not null");
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getContext(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                //startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+
+
+
+
+
         }else{
             Log.d(TAG,"result of resolve activity is  null");
         }
 
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Intent intent =  new Intent(getContext(),FilingReportActivity.class);
+        intent.putExtra(CURRENT_IMAGE_PATH,mCurrentPhotoPath);
         startActivity(intent);
     }
 }
